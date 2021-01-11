41b83acd25439633244c538cdd807be29a2c5cb6
Tudor <tudor@marghidanu.com>
2021-01-11 18:32:35 +0200

    Removed default parameters

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,4 @@ mkdir -p "${HOME}/workspace"
 
 export SHELL=/bin/bash
 export PS1="\w $ "
-exec code-server \
-    --disable-telemetry \
-    --host=0.0.0.0 \
-    "${HOME}/workspace"
+exec code-server "$@" "${HOME}/workspace"
